ba3629ba83a980071203f63c886ddfd9aa93f913
Rajamikrani <83104740+Rajamikrani@users.noreply.github.com>
2025-01-06 16:23:23 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,9 @@
+const name = "Raja Mikrani"
+const goal = "Good at Programming"
+const newStr = new String("Hello World")
+goal.toUpperCase() // if we use like it and print the goal then its not works.
+const goalUpper = goal.toUpperCase()
+
+console.log(newStr.toUpperCase())
+console.log(goalUpper.bold()) // if we use bold() method then its return in <b>value</b>
+
