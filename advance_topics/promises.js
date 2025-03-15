33cf2d40c308fe0ad093ede7ac7287a97a97fadb
Rajamikrani <83104740+Rajamikrani@users.noreply.github.com>
2025-03-15 07:36:14 +0000

    changes added

--- advance_topics/promises.js
+++ advance_topics/promises.js
@@ -64,27 +64,67 @@
 //     console.log(error)
 // })
 
+// Simple Example of using async and await
+// const promiseFive = new Promise(function(resolve , reject){
+//     setTimeout(function(){
+//         console.log("Async Task 5 Running...");
+//         let error = false;
+//         if(error){
+//             resolve("Promise Resolved")
+//         }else {
+//             reject("Promise Rejected")
+//         }
+//     } ,1000)
+// })
 
+// async function consumedPromiseFive(){
+//     try{
+//         const response = await promiseFive
+//         console.log(response)
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// }
+
+// consumedPromiseFive()
+
+// // fetch data from api using async and await
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//         const data = await response.json();
+//         console.log(data)
+//     } catch (error) {
+//         console.log("Error :" , error);
+//     }
+ 
+// }
+// getAllUsers()
+
+// Simple Example of using then method...
 const promiseFive = new Promise(function(resolve , reject){
     setTimeout(function(){
         console.log("Async Task 5 Running...");
         let error = false;
-        if(error){
+        if(!error){
             resolve("Promise Resolved")
         }else {
             reject("Promise Rejected")
         }
     } ,1000)
 })
+promiseFive
+    .then((message) => console.log(message))
+    .catch((error) => console.log(error));
 
-async function consumedPromiseFive(){
-    try{
-        const response = await promiseFive
-        console.log(response)
-    }
-    catch(error){
-        console.log(error);
-    }
-}
-
-consumedPromiseFive()
+// // fetch data from api using then and catch method..
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data)
+})
+.catch((error) => console.log(error))
+.finally("Finally Promise ended")
